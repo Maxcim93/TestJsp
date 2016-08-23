af4e3028c6e2e8da32e6b8ae7d4830592671ed36
Maksim Mekh <maxcimkj93@gmail.com>
2016-08-23 12:44:02 +0700

    Embeded tomcat integrated

--- web/src/main/java/com/maxim/web/JavaServlet.java
+++ web/src/main/java/com/maxim/web/JavaServlet.java
@@ -1,5 +1,18 @@
 package com.maxim.web;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class JavaServlet extends HttpServlet{}
+public class JavaServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // Set response content type
+        PrintWriter out = resp.getWriter();
+        out.println("Hello, Max...Pain!!");
+    }
+}
--- web/src/main/java/com/maxim/web/Main.java
+++ web/src/main/java/com/maxim/web/Main.java
@@ -1,13 +1,31 @@
 package com.maxim.web;
 
-import com.maxim.model.Registry;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
 /**
  * Created by Максим on 23.08.2016.
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
+        String webappDirLocation = "web/src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
 
-        Registry registry=new Registry();
-        System.out.println();
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/embeddedTomcat",
+                new File(webappDirLocation).getAbsolutePath());
+
+        //declare an alternate location for your "WEB-INF/classes" dir:
+        File additionWebInfClasses = new File("web/target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
